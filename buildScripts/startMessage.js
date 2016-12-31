076f0bffae1ca18b99d28e7325b104d3ac7a9408
Alistair Willis <alistairrwillis@gmail.com>
2016-12-31 14:21:49 +0900

    commit whole app

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,5 @@
+//var chalk = require(chalk);
+import chalk from 'chalk';
+
+console.log('***** STARTING APP IN DEV MODE *****');
+//console.log(chalk.green('***** STARTING APP IN DEV MODE *****'));
